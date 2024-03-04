5874c47b5c9de5679367343e38fe55299debc92c
Abbas Djaber <abbas_djaber@outlook.com>
2024-03-04 14:56:44 +0100

    .

---
+++ app/login/page.tsx
@@ -0,0 +1,59 @@
+import React from "react"
+import { useHistory } from "react-router"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+
+export default function TabsDemo() {
+  const history = useHistory()
+
+  const handleSignIn = () => {
+    // Navigate to the home page
+    history.push("/home")
+  }
+
+  return (
+    <Tabs defaultValue="account" className="w-[400px] mx-auto">
+      <TabsContent value="account">
+        <Card className="bg-green-200">
+          <CardHeader className="bg-green-500">
+            <CardTitle className="text-white">Account</CardTitle>
+            <CardDescription className="text-white">
+              Login to your account and start using our services
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="name">Matricule Uni</Label>
+              <Input id="matricule" defaultValue="matricule universitaire" />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="username">Password</Label>
+              <Input id="password" defaultValue="password" />
+            </div>
+          </CardContent>
+          <CardFooter className="bg-green-500">
+            <Button className="bg-green-700 text-white" onClick={handleSignIn}>
+              Sign in
+            </Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+    </Tabs>
+  )
+
+}
